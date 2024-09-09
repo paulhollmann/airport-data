b321e23571708f483759fbf23b0efe8d951df493
LeoKle <leon.kleinschmidt@outlook.de>
2024-09-09 13:23:05 +0200

    update validation workflow to run on PR

--- src/validate_data.py
+++ src/validate_data.py
@@ -1,6 +1,4 @@
-import os
 import sys
-import requests
 from settings import ROOT_DIRECTORY, OUTPUT_DIRECTORY
 from tasks.toml_data import TomlData
 
@@ -10,28 +8,5 @@ if __name__ == "__main__":
         data_dir=ROOT_DIRECTORY, output_dir=OUTPUT_DIRECTORY, export=False
     )
 
-    if len(tomldata.errors) == 0:
-        sys.exit(0)
-
-    GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
-    REPO_OWNER = os.getenv("REPO_OWNER")
-    REPO_NAME = os.getenv("REPO_NAME")
-    workflow_run_number = os.getenv("WORKFLOW_RUN_NUMBER")
-
-    issue_title = f"Error on workflow run {workflow_run_number}"
-    issue_body = "\n".join(tomldata.errors)
-
-    url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/issues"
-    headers = {
-        "Authorization": f"token {GITHUB_TOKEN}",
-        "Accept": "application/vnd.github.v3+json",
-    }
-    data = {"title": issue_title, "body": issue_body}
-
-    response = requests.post(url, json=data, headers=headers, timeout=5)
-
-    if response.status_code == 201:
-        print("Issue created successfully.")
-    else:
-        print(f"Failed to create issue: {response.status_code}")
-    print(response.json())
+    if len(tomldata.errors) != 0:
+        sys.exit(1)
